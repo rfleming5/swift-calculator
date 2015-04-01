0cd9828201b4b620be053b6e4a3cb471795fc93a
richard <richard@lumoslabs.com>
2015-04-01 16:54:50 -0700

    Added in the function evalutate into the CalculatorBrain.swift

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -12,16 +12,17 @@ class CalculatorBrain {
     //var opStack = Array<Op>()
     //var knownOps = Dictionary<String, Ops>()
     
-    enum Op {
+    private enum Op {
         case Operand(Double)
         case UnaryOperation(String, Double -> Double)
         case BinaryOperation(String, (Double, Double) -> Double)
     }
     
-    var opStack = [Op]()
+    // Our operation stack
+    private var opStack = [Op]()
     
     // Type dictionary
-    var knownOps = [String:Op]()
+    private var knownOps = [String:Op]()
     
     // Add lists into our dictionary
     init() {
@@ -40,5 +41,37 @@ class CalculatorBrain {
         if let operation = knownOps[symbol] {
             opStack.append(operation)
         }
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        return result
     }
 }
